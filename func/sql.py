1952e5b5dc18016702a86c940d9eb57d2f6dbd36
dragoon1251996 <whiyew@gmail.com>
2019-06-20 15:00:42 +0700

    Update sql.py

--- func/sql.py
+++ func/sql.py
@@ -1,3 +1,3 @@
 sql = """UPDATE questions SET `general` = %s WHERE id = %s"""
-sql = """select * from table 
-order by FIELD(Code, 'Health', 'Phone', 'Freeze', 'Hot')"""
+sql = """select * from table where id in (1,2,3,4,5)
+order by FIELD(id, 3,4,5,1,2)"""
