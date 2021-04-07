d7f4a70bb684250a077ff7a389fa3dab21bd9089
dragoon1251996 <whiyew@gmail.com>
2021-04-07 15:20:14 +0700

    Update sql.py

--- func/sql.py
+++ func/sql.py
@@ -1,3 +1,4 @@
 sql = """UPDATE questions SET `general` = %s WHERE id = %s"""
 sql = """select * from table where id in (1,2,3,4,5)
 order by FIELD(id, 3,4,5,1,2)"""
+"UPDATE movies SET rating = ? WHERE name = ?", (8.7, "'Allo 'Allo! (1982)")
